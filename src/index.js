1f8757609e16030235e9b0a30f432b103c989d97
Andrey Popp <8mayday@gmail.com>
2015-09-09 01:16:01 +0300

    feat(*): export Style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 
 import stylesheet  from './createStylesheet';
 import style       from './styleComponent';
+import Style       from './Style';
 import Styleable   from './Styleable';
 
-export {stylesheet, style, Styleable};
+export {stylesheet, style, Styleable, Style};
 export default Styleable;
