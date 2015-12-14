dfc2d2821a86338637cfb95067b3daec8247328e
Andrey Popp <8mayday@gmail.com>
2015-12-14 13:32:14 +0300

    refactor: move DOM stylesheet to a separate package

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@
  * @copyright 2015 Prometheus Research, LLC
  */
 
+import invariant from 'invariant';
+import isValidReactComponent from './isValidReactComponent';
+
 export attachStylesheet from './attachStylesheet';
 export styleComponent from './styleComponent';
 export {createStylesheet} from './Stylesheet';
 
 export styleable from './styleable';
+
+/**
+ * Apply a stylesheet to a component.
+ */
+export default function style(Component, stylesheet, styleDOMComponent = null) {
+  invariant(
+    isValidReactComponent(Component),
+    'Expected a valid React component, got: %s',
+    typeof Component
+  );
+  if (typeof Component.style === 'function') {
+    return Component.style(stylesheet);
+  } else if (styleDOMComponent) {
+    return styleDOMComponent(Component, stylesheet);
+  } else {
+    invariant(
+      false,
+      'Unable to style component'
+    );
+  }
+}
