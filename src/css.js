7caa11098f0df208c86fad09f152f07e1f05bbfc
Andrey Popp <8mayday@gmail.com>
2017-08-28 15:34:26 +0300

    feat: more types

--- src/css.js
+++ src/css.js
@@ -13,15 +13,164 @@ export type CSSMeasure = number | CSSMeasureUnit;
 export type CSSDisplay = 'block' | 'inline-block' | 'flex' | 'inline-flex' | 'inline';
 export type CSSPosition = 'absolute' | 'relative' | 'fixed' | 'sticky';
 
+export type CSSAll = 'initial' | 'inherit' | 'unset';
+
+export type CSSAppearance = 'auto' | 'none';
+
+export type CSSBackfaceVisibility = 'visible' | 'hidden';
+
+export type CSSBoxSizing = 'content-box' | 'border-box';
+
+export type CSSClear = 'none' | 'left' | 'right' | 'both' | 'inline-start' | 'inline-end';
+
+export type CSSFloat = 'left' | 'right' | 'none' | 'inline-start' | 'inline-end';
+
+export type CSSFlexBasis = 'auto' | CSSMeasure;
+export type CSSFlexGrow = number;
+export type CSSFlexShrink = number;
+export type CSSFlexDirection = 'row' | 'row-reverse' | 'column' | 'column-reverse';
+export type CSSFlexWrap = 'nowrap' | 'wrap' | 'wrap-reverse';
+export type CSSJustifyContent =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around';
+export type CSSAlignContent =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'stretch';
+export type CSSAlignItems = 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
+export type CSSAlignSelf =
+  | 'auto'
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'baseline'
+  | 'stretch';
+export type CSSOrder = number;
+
+export type CSSCursor =
+  | 'auto'
+  | 'default'
+  | 'none'
+  | 'context-menu'
+  | 'help'
+  | 'pointer'
+  | 'progress'
+  | 'wait'
+  | 'cell'
+  | 'crosshair'
+  | 'text'
+  | 'vertical-text'
+  | 'alias'
+  | 'copy'
+  | 'move'
+  | 'no-drop'
+  | 'not-allowed'
+  | 'e-resize'
+  | 'n-resize'
+  | 'ne-resize'
+  | 'nw-resize'
+  | 's-resize'
+  | 'se-resize'
+  | 'sw-resize'
+  | 'w-resize'
+  | 'ew-resize'
+  | 'ns-resize'
+  | 'nesw-resize'
+  | 'nwse-resize'
+  | 'col-resize'
+  | 'row-resize'
+  | 'all-scroll'
+  | 'zoom-in'
+  | 'zoom-out'
+  | 'grab'
+  | 'grabbing';
+
+export type CSSDirection = 'ltr' | 'rtl';
+
+export type CSSFontWeight =
+  | 'normal'
+  | 'bold'
+  | 'bolder'
+  | 'lighter'
+  | 100
+  | 200
+  | 300
+  | 400
+  | 500
+  | 600
+  | 700
+  | 800
+  | 900;
+
+export type CSSOpacity = number;
+
+export type CSSOverflow = 'visible' | 'hidden' | 'scroll' | 'auto';
+
+export type CSSPointerEvents =
+  | 'auto'
+  | 'none'
+  | 'visiblePainted'
+  | 'visibleFill'
+  | 'visibleStroke'
+  | 'visible'
+  | 'painted'
+  | 'fill'
+  | 'stroke'
+  | 'all'
+  | 'inherit';
+
+export type CSSResize = 'none' | 'both' | 'horizontal' | 'vertical';
+
+export type CSSUserSelect = 'auto' | 'text' | 'none' | 'contain' | 'all';
+
+export type CSSVerticalAlign =
+  | 'baseline'
+  | 'sub'
+  | 'super'
+  | 'text-top'
+  | 'text-bottom'
+  | 'middle'
+  | 'top'
+  | 'bottom'
+  | string
+  | number;
+
+export type CSSVisibility = 'visible' | 'hidden' | 'collapse';
+
+export type CSSWhiteSpace = 'normal' | 'pre' | 'nowrap' | 'pre-wrap' | 'pre-line';
+
+export type CSSZIndex = number | 'auto';
+
+export type CSSTextAlign =
+  | 'start'
+  | 'end'
+  | 'left'
+  | 'right'
+  | 'center'
+  | 'justify'
+  | 'match-parent';
+
+export type CSSUntypedString = string;
+
 export type CSSPropertySet = {
   color?: ?CSSColor,
-  backgroundColor?: ?CSSColor,
 
   display?: ?CSSDisplay,
   position?: ?CSSPosition,
 
   width?: ?CSSMeasure,
+  minWidth?: ?CSSMeasure,
+  maxWidth?: ?CSSMeasure,
+
   height?: ?CSSMeasure,
+  minHeight?: ?CSSMeasure,
+  maxHeight?: ?CSSMeasure,
 
   top?: ?CSSMeasure,
   bottom?: ?CSSMeasure,
@@ -50,7 +199,147 @@ export type CSSPropertySet = {
   marginVertical?: ?CSSMeasure,
   marginHorizontal?: ?CSSMeasure,
 
-  // TODO: add more here
+  all?: ?CSSAll,
+
+  animation?: ?CSSUntypedString,
+  animationDelay?: ?CSSUntypedString,
+  animationDirection?: ?CSSUntypedString,
+  animationDuration?: ?CSSUntypedString,
+  animationFillMode?: ?CSSUntypedString,
+  animationIterationCount?: ?CSSUntypedString,
+  animationName?: ?CSSUntypedString,
+  animationPlayState?: ?CSSUntypedString,
+  animationTimingFunction?: ?CSSUntypedString,
+
+  appearance?: ?CSSAppearance,
+  backdropFilter?: ?CSSUntypedString,
+  backfaceVisibility?: ?CSSBackfaceVisibility,
+
+  background?: ?CSSUntypedString,
+  backgroundAttachment?: ?CSSUntypedString,
+  backgroundBlendMode?: ?CSSUntypedString,
+  backgroundClip?: ?CSSUntypedString,
+  backgroundColor?: ?CSSColor,
+  backgroundImage?: ?CSSUntypedString,
+  backgroundOrigin?: ?CSSUntypedString,
+  backgroundPosition?: ?CSSUntypedString,
+  backgroundPositionX?: ?CSSUntypedString,
+  backgroundPositionY?: ?CSSUntypedString,
+  backgroundRepeat?: ?CSSUntypedString,
+  backgroundSize?: ?CSSUntypedString,
+
+  borderRadius?: ?CSSUntypedString,
+  borderBottomLeftRadius?: ?CSSUntypedString,
+  borderBottomRightRadius?: ?CSSUntypedString,
+  borderTopLeftRadius?: ?CSSUntypedString,
+  borderTopRightRadius?: ?CSSUntypedString,
+
+  borderCollapse?: ?CSSUntypedString,
+  borderSpacing?: ?CSSUntypedString,
+
+  border?: ?CSSUntypedString,
+  borderColor?: ?CSSUntypedString,
+  borderStyle?: ?CSSUntypedString,
+  borderWidth?: ?CSSUntypedString,
+
+  borderBottom?: ?CSSUntypedString,
+  borderBottomColor?: ?CSSUntypedString,
+  borderBottomStyle?: ?CSSUntypedString,
+  borderBottomWidth?: ?CSSUntypedString,
+
+  borderLeft?: ?CSSUntypedString,
+  borderLeftColor?: ?CSSUntypedString,
+  borderLeftStyle?: ?CSSUntypedString,
+  borderLeftWidth?: ?CSSUntypedString,
+
+  borderRight?: ?CSSUntypedString,
+  borderRightColor?: ?CSSUntypedString,
+  borderRightStyle?: ?CSSUntypedString,
+  borderRightWidth?: ?CSSUntypedString,
+
+  borderTop?: ?CSSUntypedString,
+  borderTopColor?: ?CSSUntypedString,
+  borderTopStyle?: ?CSSUntypedString,
+  borderTopWidth?: ?CSSUntypedString,
+
+  boxShadow?: ?CSSUntypedString,
+
+  boxSizing?: ?CSSBoxSizing,
+
+  float?: ?CSSFloat,
+  clear?: ?CSSClear,
+
+  content?: ?CSSUntypedString,
+
+  cursor?: ?CSSCursor,
+  direction?: ?CSSDirection,
+
+  flex?: ?CSSUntypedString,
+  flexBasis?: ?CSSFlexBasis,
+  flexDirection?: ?CSSFlexDirection,
+  flexFlow?: ?CSSUntypedString,
+  flexGrow?: ?CSSFlexGrow,
+  flexShrink?: ?CSSFlexShrink,
+  flexWrap?: ?CSSFlexWrap,
+  justifyContent?: ?CSSJustifyContent,
+  alignItems?: ?CSSAlignItems,
+  alignSelf?: ?CSSAlignSelf,
+  alignContent?: ?CSSAlignContent,
+  order?: ?CSSOrder,
+
+  font?: ?CSSUntypedString,
+  fontFamily?: ?CSSUntypedString,
+  fontSize?: ?CSSUntypedString,
+  fontStyle?: ?CSSUntypedString,
+  fontWeight?: ?CSSFontWeight,
+
+  lineHeight?: ?CSSUntypedString,
+
+  listStyle?: ?CSSUntypedString,
+  listStyleImage?: ?CSSUntypedString,
+  listStylePosition?: ?CSSUntypedString,
+  listStyleType?: ?CSSUntypedString,
+
+  opacity?: ?CSSOpacity,
+
+  outline?: ?CSSUntypedString,
+  outlineColor?: ?CSSUntypedString,
+  outlineOffset?: ?CSSUntypedString,
+  outlineStyle?: ?CSSUntypedString,
+  outlineWidth?: ?CSSUntypedString,
+
+  overflow?: ?CSSOverflow,
+  overflowX?: ?CSSOverflow,
+  overflowY?: ?CSSOverflow,
+
+  pointerEvents?: ?CSSPointerEvents,
+  resize?: ?CSSResize,
+
+  textAlign?: ?CSSTextAlign,
+  textDecoration?: ?CSSUntypedString,
+  textOverflow?: ?CSSUntypedString,
+  textShadow?: ?CSSUntypedString,
+  textTransform?: ?CSSUntypedString,
+
+  transform?: ?CSSUntypedString,
+  transformBox?: ?CSSUntypedString,
+  transformOrigin?: ?CSSUntypedString,
+  transformStyle?: ?CSSUntypedString,
+  transition?: ?CSSUntypedString,
+  transitionDelay?: ?CSSUntypedString,
+  transitionDuration?: ?CSSUntypedString,
+  transitionProperty?: ?CSSUntypedString,
+  transitionTimingFunction?: ?CSSUntypedString,
+
+  userSelect?: ?CSSUserSelect,
+
+  verticalAlign?: ?CSSVerticalAlign,
+
+  visibility?: ?CSSVisibility,
+
+  whiteSpace?: ?CSSWhiteSpace,
+
+  zIndex?: ?CSSZIndex,
 };
 
 export function em(val: number): CSSMeasureUnit {
